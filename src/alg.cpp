680fd5e5a767d32d0f31109d3fa13f3e9634fb0d
GurenkoNick <78843007+GurenkoNick@users.noreply.github.com>
2021-04-22 02:15:01 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,13 +1,73 @@
 // Copyright 2021 NNTU-CS
 #include <string>
 #include "tstack.h"
+using namespace std;
 
-std::string infx2pstfx(std::string inf) {
-  // добавьте сюда нужный код
-  return std::string("");
+int pr(char x) {
+    if ((x == '-') || (x == '+'))
+        return 2;
+    else if ((x == '/') || (x == '*'))
+        return 3;
+    else
+        return 0;
 }
-
+std::string infx2pstfx(std::string inf) {
+  Tstack<char> stack;
+    string str;
+    for (int i = 0; i < inf.length(); i++) {
+        if ((inf[i] >= '0') && (inf[i] <= '9')) {
+            str += inf[i];
+            str += " ";
+        }
+        else if ((stack.empty()) || (inf[i] == '(') || (pr(inf[i] > pr(stack.top())))) {
+            stack.push(inf[i]);
+        }
+        else if (inf[i] == ')') {
+            while (stack.top() != '(') {
+                str += stack.top();
+                str += ' ';
+                stack.pop();
+            }
+            stack.pop();
+        }
+        else if (!stack.empty())  {
+            while (pr(inf[i] <= pr(stack.top()))) {
+                str += stack.top();
+                str += ' ';
+                stack.pop();
+            }
+            stack.push(inf[i]);
+        }
+    }
+     if (!stack.empty())
+         while (!stack.empty()) {
+            str += stack.top();
+            str += ' ';
+            stack.pop();
+        }
+    return str;
+}
 int eval(std::string pst) {
-  // добавьте сюда нужный код
-  return 0;
+  Tstack<char> stack;
+  int result, n1, n2;
+        for (int i = 0; i < pst.length(); i++) {
+            if ((pst[i] >= '0') && (pst[i] <= '9'))
+                stack.push(pst[i] - '0');
+            else if (pst[i] != ' ') {
+                n2 = stack.top();
+                stack.pop();
+                n1 = stack.top();
+                stack.pop();
+                if (pst[i] == '+')
+                    stack.push(n1 + n2);
+                if (pst[i] == '-')
+                    stack.push(n1 - n2);
+                if (pst[i] == '*')
+                    stack.push(n1 * n2);
+                if (pst[i] == '/')
+                    stack.push(n1 / n2);
+            }
+        }
+        result = stack.top();
+        return result;
 }
