54635b20f5ecd15972e704a4859333104d37a212
GurenkoNick <78843007+GurenkoNick@users.noreply.github.com>
2021-04-22 02:34:27 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,7 +1,6 @@
 // Copyright 2021 NNTU-CS
 #include <string>
-#include "tstack.h"
-using namespace std;
+#include <stack>
 
 int pr(char x) {
     if ((x == '-') || (x == '+'))
@@ -12,47 +11,56 @@ int pr(char x) {
         return 0;
 }
 std::string infx2pstfx(std::string inf) {
-  TStack<char> stack;
+  stack<char> stack;
     string str;
     for (int i = 0; i < inf.length(); i++) {
         if ((inf[i] >= '0') && (inf[i] <= '9')) {
             str += inf[i];
             str += " ";
         }
-        else if ((stack.empty()) || (inf[i] == '(') || (pr(inf[i] > pr(stack.top())))) {
+        else {
+            if ((stack.empty()) || (inf[i] == '(') || (pr(inf[i] > pr(stack.top())))) {
             stack.push(inf[i]);
+            }
         }
-        else if (inf[i] == ')') {
+        else {
+            if (inf[i] == ')') {
             while (stack.top() != '(') {
                 str += stack.top();
                 str += ' ';
                 stack.pop();
             }
             stack.pop();
+            }
         }
-        else if (!stack.empty())  {
+        
+        else {
+            if (!stack.empty())  {
             while (pr(inf[i] <= pr(stack.top()))) {
                 str += stack.top();
                 str += ' ';
                 stack.pop();
             }
             stack.push(inf[i]);
+            }
         }
     }
-     if (!stack.empty())
+     if (!stack.empty()) {
          while (!stack.empty()) {
             str += stack.top();
             str += ' ';
             stack.pop();
         }
+     }
     return str;
 }
 int eval(std::string pst) {
-  TStack<char> stack;
+  stack<char> stack;
   int result, n1, n2;
         for (int i = 0; i < pst.length(); i++) {
-            if ((pst[i] >= '0') && (pst[i] <= '9'))
+            if ((pst[i] >= '0') && (pst[i] <= '9')) {
                 stack.push(pst[i] - '0');
+            }
             else if (pst[i] != ' ') {
                 n2 = stack.top();
                 stack.pop();
