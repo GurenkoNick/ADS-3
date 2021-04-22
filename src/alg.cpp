a6fd69ae3d62ff90c8fea9202fb8bd78cd73c344
GurenkoNick <78843007+GurenkoNick@users.noreply.github.com>
2021-04-22 13:38:24 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -18,11 +18,11 @@ std::string infx2pstfx(std::string inf) {
             str += inf[i];
             str += " ";
         }
-        else if ((stack.empty()) || (inf[i] == '(') 
+         else if ((stack.empty()) || (inf[i] == '(')
                  || (pr(inf[i] > pr(stack.top())))) {
             stack.push(inf[i]);
         }
-        else if (inf[i] == ')') {
+         else if (inf[i] == ')') {
             while (stack.top() != '(') {
                 str += stack.top();
                 str += ' ';
@@ -30,7 +30,7 @@ std::string infx2pstfx(std::string inf) {
             }
             stack.pop();
         }
-        else {
+         else {
             while (!stack.empty() && (pr(stack.top()) >= pr(inf[i]))) {
                 str += stack.top();
                 str += ' ';
@@ -39,12 +39,13 @@ std::string infx2pstfx(std::string inf) {
             stack.push(inf[i]);
         }
     }
-     if (!stack.empty())
+     if (!stack.empty()) {
          while (!stack.empty()) {
             str += stack.top();
             str += ' ';
             stack.pop();
          }
+     }
     str.pop_back();
     return str;
 }
@@ -55,7 +56,7 @@ int eval(std::string pst) {
             if ((pst[i] >= '0') && (pst[i] <= '9')) {
                 stack.push(pst[i] - '0');
             }
-            else if (pst[i] != ' ') {
+             else if (pst[i] != ' ') {
                 n2 = stack.top();
                 stack.pop();
                 n1 = stack.top();
